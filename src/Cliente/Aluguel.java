f20fed0a29eaa70aeb9b8d849b695cd4e6db6f0f
yasserPr10 <127783452+yasserPr10@users.noreply.github.com>
2023-04-04 10:25:29 +0200

    Update Aluguel.java

--- src/Cliente/Aluguel.java
+++ src/Cliente/Aluguel.java
@@ -4,13 +4,7 @@
  */
 package Cliente;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
